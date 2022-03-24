673ff36d08c37248ebb41ef3955990e5455aad14
LizardLiang <shotup0101@gmail.com>
2022-03-24 16:37:59 +0800

    I finally can sleep better now

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import theme from "./lib/theme";
+import { ChakraProvider } from "@chakra-ui/react";
+import { AnimatePresence } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <AnimatePresence exitBeforeEnter initial={true}>
+        <Component {...pageProps} />
+      </AnimatePresence>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
