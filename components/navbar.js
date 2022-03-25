ef51329a89f2957087808a3be81f568128b8ac03
LizardLiang <shotup0101@gmail.com>
2022-03-25 16:23:20 +0800

    add navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,45 +1,40 @@
-import NextLink from "next/link";
+import NextLink from 'next/link'
 import {
   Container,
   Box,
   Link,
   Stack,
-  Heading,
-  Flex,
-  Menu,
-  MenuItem,
-  MenuList,
-  MenuButton,
-  IconButton,
-  useColorModeValue,
-} from "@chakra-ui/react";
-
-import { HanburgerIcon } from "@chakra-ui/icons";
+  Button,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const LinkItem = ({ href, path, target, children, ...props }) => {
-  const active = path === href;
-  const inactiveColor = useColorModeValue("gray.200", "whiteAlpha.900");
+  const active = path === href
+  const inactiveColor = useColorModeValue('gray.200', 'whiteAlpha.900')
 
   return (
     <Link
       p={2}
-      bg={active ? "grassTeak" : undefined}
-      color={active ? "#202023" : inactiveColor}
+      bg={active ? 'grassTeal' : undefined}
+      color={active ? '#202023' : inactiveColor}
+      borderRadius="md"
       {...props}
     >
       {children}
     </Link>
-  );
-};
+  )
+}
 
-const Navbar = (props) => {
+const Navbar = props => {
   return (
     <Box
       position="fixed"
       as="nav"
       w="100%"
-      bg={useColorModeValue("#ffffff40", "#20202380")}
-      css={{ backdropFilter: "blur(10px)" }}
+      bg={useColorModeValue('#ffffff40', '#20202380')}
+      css={{ backdropFilter: 'blur(10px)' }}
       zIndex={1}
       {...props}
     >
@@ -52,10 +47,10 @@ const Navbar = (props) => {
         justify="space-between"
       >
         <Stack
-          direction={{ base: "column", md: "row" }}
-          display={{ base: "none", md: "flex" }}
-          width={{ base: "full", md: "auto" }}
-          alignItem="center"
+          direction="row"
+          display="flex"
+          width={{ base: 'full', md: 'auto' }}
+          alignItems="center"
           flexGrow={1}
           mt={{ base: 4, md: 0 }}
         >
@@ -67,11 +62,14 @@ const Navbar = (props) => {
           >
             語音包
           </LinkItem>
+          <Box display="flex" flexGrow={1} justifyContent="flex-end">
+            <Button leftIcon={<FontAwesomeIcon icon={faGithub} />}>源碼</Button>
+          </Box>
         </Stack>
       </Container>
     </Box>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
 
