03b366da911d1b29eeade2fd4023c8fadfffddd6
LizardLiang <shotup0101@gmail.com>
2022-03-26 20:10:32 +0800

    Fix Navbar

--- components/main.js
+++ components/main.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { Box, Container } from '@chakra-ui/react'
-import Navbar from '../navbar'
+import Navbar from './navbar'
 
 const Main = ({ children, router }) => {
   return (
