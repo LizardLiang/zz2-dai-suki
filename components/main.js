2985b3b17031b77a0826071a6ca96cdc30f78671
LizardLiang <shotup0101@gmail.com>
2022-03-26 20:17:25 +0800

    Fix Navbar

--- components/main.js
+++ components/main.js
@@ -7,7 +7,7 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Lizard - Animal Adoption</title>
+        <title>zz2 我婆</title>
       </Head>
       <Navbar />
       <Container maxW="container.md" pt={14}>
