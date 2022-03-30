22d1e2d9188e3adfcb162a1877c1aa4db1019f2b
LizardLiang <shotup0101@gmail.com>
2022-03-30 19:33:25 +0800

    Add 3D zz2 lego

--- components/main.js
+++ components/main.js
@@ -1,6 +1,13 @@
 import Head from 'next/head'
 import { Box, Container } from '@chakra-ui/react'
+import dynamic from 'next/dynamic'
 import Navbar from './navbar'
+import VoxelDogLoader from './voxel-zz2-loader'
+
+const LazyVoxelZz2 = dynamic(() => import('./voxel-zz2'), {
+  ssr: false,
+  loading: () => <VoxelDogLoader />
+})
 
 const Main = ({ children }) => {
   return (
@@ -11,10 +18,10 @@ const Main = ({ children }) => {
       </Head>
       <Navbar />
       <Container maxW="container.md" pt={14}>
+        <LazyVoxelZz2 />
         {children}
       </Container>
     </Box>
   )
 }
 export default Main
-
